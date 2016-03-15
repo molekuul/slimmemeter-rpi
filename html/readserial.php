34edce2929fa30a6e05dac71e3fefdf2899387db
Ge Janssen <github@gejanssen.com>
2016-03-15 22:11:59 +0100

    php pagina's toegevoegd

---
+++ html/readserial.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <pre>
+<?php
+#$command=system("/bin/df");
+#$command=system("/usr/bin/python /home/gej/P1uitlezer.py 2>&1");
+$command=system("/usr/bin/python /home/gej/P1uitlezer.py");
+#echo $command;
+
+?>
+    </pre><!-- sorry.. josn ;-) -->
+  </body>
+</html>
---
+++ html/readserialplain.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    <pre>
+<?php
+#$command=system("/usr/bin/python /home/gej/P1uitlezer.py 2>&1");
+$command=system("/usr/bin/python /home/gej/P1uitlezen.py");
+#echo $command;
+
+?>
+    </pre><!-- sorry.. josn ;-) -->
+  </body>
+</html>
